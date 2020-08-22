cdbaa47397022dccaa27ea2c959c34d54e3e919d
toomec2105 <toomec2105@interia.pl>
2020-08-22 17:27:31 +0200

    add cores

--- src/main/java/com/tomek/web_jpa_2/user/UserController.java
+++ src/main/java/com/tomek/web_jpa_2/user/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:1234")
 @RestController // = @Controller @ResponseBody
 @RequestMapping("users") // modifies the path for each method in this class
 //@RequestMapping maps all http methods
